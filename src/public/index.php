71225069c8ca4a359ec04e0c58aa2d2b09bac789
MARECHAL Armand <Armand.MARECHAL@cpnv.ch>
2021-10-05 15:08:00 +0200

    Update index.php

--- src/public/index.php
+++ src/public/index.php
@@ -7,26 +7,36 @@ require_once APP_ROOT . '/.env.php';
 use \App\Models\Members;
 use \App\Controllers\HomeController;
 use App\Controllers\MembersController;
+use App\Controllers\MyTeamsController;
+use App\Controllers\TeamController;
 
 $_SESSION['userLog'] = Members::find(USER_ID); // Armand
 
-if(!isset($_GET['view']))
-{
+if (!isset($_GET['view'])) {
     $view = 'home';
 } else {
     $view = $_GET['view'];
 }
 
-switch($view)
-{
+switch ($view) {
     case 'home': {
-        (new HomeController())->index();
-        break;
-    }
+            (new HomeController())->index();
+            break;
+        }
     case 'membres': {
-        (new MembersController())->index();
-        break;
-    }
+            (new MembersController())->index();
+            break;
+        }
+    case 'my-teams': {
+            (new MyTeamsController())->index();
+            break;
+        }
+    case 'team': {
+            (new TeamController())->index();
+            break;
+        }
+    default: {
+            (new HomeController())->index();
+            break;
+        }
 }
-
-
