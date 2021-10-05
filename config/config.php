665dd84e3dcb08c9cfeb4ce59117140eaf1ba77d
MARECHAL Armand <Armand.MARECHAL@cpnv.ch>
2021-10-05 12:51:10 +0200

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+//site name
+define('SITE_NAME', 'ExerciseLooper');
+
+//App Root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+define('URL_ROOT', '/public');
+define('VIEW_ROOT', APP_ROOT . '/resources/views');
