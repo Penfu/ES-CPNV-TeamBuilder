e4ee0dcfea8798920ba9e70d88e3848abe766d05
MARECHAL Armand <Armand.Marechal@cpnv.ch>
2021-10-12 10:47:58 +0200

    Create .env-example.php

---
+++ src/.env-example.php
@@ -0,0 +1,9 @@
+<?php
+
+const DB_HOST = 'hostname';
+const DB_PORT = 0000;
+const DB_DATABASE = 'dbname';
+const DB_USERNAME = 'dbusername';
+const DB_PASSWORD = 'dbpassword';
+
+const USER_ID = 1;
