4adf498c2f3e2a5610fa5b3a357858f041451c51
MARECHAL Armand <Armand.MARECHAL@cpnv.ch>
2021-11-12 12:16:39 +0100

    End Examen

--- src/resources/views/layout.php
+++ src/resources/views/layout.php
@@ -25,7 +25,7 @@ use App\Controllers\Components\NavbarController,
             <?php if (isset($_SESSION['alert'])) : ?>
                 <?= (new BannerController)->index() ?>
             <?php endif ?>
-            <h3 class="container max-w-7xl mx-auto my-4 px-2 sm:px-6 lg:px-8">Version: Début Examen - Armand</h3>
+            <h3 class="container max-w-7xl mx-auto my-4 px-2 sm:px-6 lg:px-8">Version: Fin Examen - Armand</h3>
             <?= $content ?>
         </div>
     </div>
